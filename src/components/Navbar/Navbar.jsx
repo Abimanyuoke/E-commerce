2a9b9c2a1aefaeb934dd9a87916309c7351becb9
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-11-23 16:06:03 +0700

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+        {/* upper Navbar */}
+        <div className="">
+            <div className=""></div>
+        </div>
+        {/* lower Navbar */}
+        <div className=""></div>
+    </div>
+  )
+}
+
+export default Navbar
