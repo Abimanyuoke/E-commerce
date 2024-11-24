13fd80aadd1835c141c94c38e0d1bbc8b42ab56d
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-11-24 16:33:36 +0700

    add card sections

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -8,15 +8,15 @@ import Slider from "react-slick"
 const ImageList = [
     {
         id: 1,
-        img: Image1,
-        title: "Upto 50% off on all Men's Wear",
+        img: Image2,
+        title: "30% off on all Women's Wear",
         description: 
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis exercitationem facere dolore delectus rem architecto ducimus sint necessitatibus ex voluptat."
     },
     {
         id: 2,
-        img: Image2,
-        title: "30% off on all Women's Wear",
+        img: Image1,
+        title: "Upto 50% off on all Men's Wear",
         description: 
         "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis exercitationem facere dolore delectus rem architecto ducimus sint necessitatibus ex voluptat."
     },
@@ -55,15 +55,30 @@ const Hero = () => {
                 <div className='grid grid-cols-1   sm:grid-cols-2'>
                      {/* text content section */}
                     <div className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
-                        <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
-                        <p className='text-sm'>{data.description}</p>
-                        <div>
+                        <h1 
+                        data-aos="zoom-out"
+                        data-aos-duration="500"
+                        data-aos-once="true"
+                        className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
+                        <p 
+                        data-aos="fade-up"
+                        data-aos-duration="500"
+                        data-aos-delay="100"
+                        className='text-sm'>{data.description}</p>
+                        <div
+                        data-aos="fade-up"
+                        data-aos-duration="500"
+                        data-aos-delay="300"
+                        >
                             <button className='bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>Order Now</button>
                         </div>
                     </div>
                      {/* Image section */}
                     <div className='order-1 sm:order-2'>
-                        <div className='relative z-10'>
+                        <div 
+                        data-aos="zoom-in"
+                        data-aos-once="true"
+                        className='relative z-10'>
                             <img src={data.img} alt="" className='w-[300px] h-[300px] sm:w-[450px] sm:h-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto'/>
                         </div>
                     </div>
