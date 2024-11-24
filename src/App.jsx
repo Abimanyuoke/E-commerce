7c0d2c3458ec1b99202569d37d2f0307551762a5
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-11-24 15:28:48 +0700

    fix the problem

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar';
 import Hero from './components/Hero/Hero';
-import Products from './components/Products/Products';
+
 
 
 
@@ -10,7 +10,6 @@ const App = () => {
   <div>
     <Navbar/>
     <Hero/>
-    <Products/>
   </div>
   )
 };
--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
-import Image1 from  "../../assets/hero/women.png"
-import Image2 from  "../../assets/hero/shopping.png"
-import Image3 from  "../../assets/hero/sale.png"
+import Image1 from  "../../assets/hero/shopping.png";
+import Image2 from  "../../assets/hero/women.png";
+import Image3 from  "../../assets/hero/sale.png";
+import Slider from "react-slick"
 
 
 const ImageList = [
@@ -29,7 +30,20 @@ const ImageList = [
 ]
 
 const Hero = () => {
-return (
+
+    var settings = {
+        dots: false,
+        arrows: false,
+        speed: 800,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 4000,
+        cssEase: "ease-in-out",
+        pauseOnHover: false,
+        pauseOnFocus: true,
+    };
+
+  return (
     <div className='relative overflow-hidden min-h-[550px] sm:min-h-[650px] bg-gray-100 flex justify-center items-center dark:bg-gray-950 dark:text-white duration-200'>
         {/* background pattern */}
         <div className='h-[700px] w-[700px] bg-primary/40 absolute -top-1/2 right-0 rounded-3xl rotate-45 -z-9'></div>
@@ -43,9 +57,9 @@ return (
                     <div className='flex flex-col justify-center gap-4 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10'>
                         <h1 className='text-5xl sm:text-6xl lg:text-7xl font-bold'>{data.title}</h1>
                         <p className='text-sm'>{data.description}</p>
-                        <dir>
+                        <div>
                             <button className='bg-gradient-to-r from-primary to-secondary hover:scale-105 duration-200 text-white py-2 px-4 rounded-full'>Order Now</button>
-                        </dir>
+                        </div>
                     </div>
                      {/* Image section */}
                     <div className='order-1 sm:order-2'>
@@ -59,7 +73,7 @@ return (
         </Slider>
         </div>
     </div>
-)
+  )
 }
 
 export default Hero
