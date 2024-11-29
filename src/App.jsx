6485a17ccb8d10b1262e6af0ba171197414957fa
Abimanyuoke123 <fananiabimanyu@gmail.com>
2024-11-29 22:02:36 +0700

    add Subscribe

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import AOS from "aos";
 import "aos/dist/aos.css"
 import TopProducts from './components/TopProducts/TopProducts';
 import Banner from './components/Banner/Banner';
+import Subscribe from './components/Subscribe/Subscribe';
 
 
 
@@ -29,6 +30,8 @@ const App = () => {
     <Products/>
     <TopProducts/>
     <Banner/>
+    <Subscribe/>
+    <Products/>
   </div>
   )
 };
