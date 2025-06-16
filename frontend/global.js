fecec89077ea0993785d53b5fa656857ec2bf4ad
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-06-16 14:23:59 +0700

    update environment configuration and add API constants

---
+++ frontend/global.js
@@ -0,0 +1,5 @@
+export const BASE_API_URL = process.env.NEXT_PUBLIC_BASE_API_URL
+export const BASE_IMAGE_PROFILE = `${BASE_API_URL}/profile_picture`
+export const BASE_IMAGE_SHIRT = `${BASE_API_URL}/shirt_picture`
+export const BASE_IMAGE_PANTS = `${BASE_API_URL}/pants_picture`
+export const BASE_IMAGE_HAT = `${BASE_API_URL}/hat_picture`
