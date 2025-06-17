73b4b46090478db6b9a3f5b0d958590dfd77caa8
Abimanyuoke123 <fananiabimanyu@gmail.com>
2025-06-17 21:23:46 +0700

    feat: Add new components for file input, footer, modal, navbar, and user interface
    - Implemented FileInput component for file uploads with validation.
    - Created Footer and FooterLogin components for consistent page footers.
    - Developed Modal component for displaying overlays.
    - Added Navbar and NavbarLogin components for navigation.
    - Introduced Payment and Select components for user interactions.
    - Built landing and main page layouts with integrated user components.
    - Designed user-facing components including Banner, Hero, Products, and Testimonials.
    - Implemented dark mode toggle functionality in Navbar.
    - Created Popup component for order forms.
    - Enhanced Subscribe and TopProducts components for user engagement.

---
+++ frontend/app/main/page.tsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import Navbar from '../user/Navbar/page';
+import Hero from '../user/Hero/page';
+import Products from '../user/Products/page';
+import AOS from "aos";
+import "aos/dist/aos.css"
+import TopProducts from '../user/TopProducts/page';
+import Banner from '../user/Banner/page';
+import Subscribe from '../user/Subscribe/page';
+import Testimonials from '../user/Testimonials/page';
+import Footer from '../user/Footer/page';
+import Popup from '../user/Popup/page';
+
+
+
+const Main = () => {
+    const [orderPopup, setOrderPopup] = React.useState(false);
+    const handleOrderPopup = () => {
+        setOrderPopup(!orderPopup);
+    };
+
+    React.useEffect(() => {
+        AOS.init({
+            offset: 100,
+            duration: 800,
+            easing: "ease-in-out",
+            delay: 100,
+        });
+        AOS.refresh();
+    }, []);
+
+
+    return (
+        <div className='bg-white dark:bg-gray-900 dark:text-white duration-200'>
+            <Navbar handleOrderPopup={handleOrderPopup} />
+            <Hero handleOrderPopup={handleOrderPopup} />
+            <Products />
+            <TopProducts handleOrderPopup={handleOrderPopup} />
+            <Banner />
+            <Subscribe />
+            <Products />
+            <Testimonials />
+            <Footer />
+            <Popup orderPopup={orderPopup} setOrderPopup={setOrderPopup} />
+        </div>
+    )
+};
+
+export default Main
