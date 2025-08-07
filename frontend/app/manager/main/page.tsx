614e1ef149d4d01a6c02067a2f2e4418c51db165
Fanani Akbar Abimanyu <fananiabimanyu@gmail.com>
2025-08-07 21:03:43 +0700

    `Added lines 4-5 to file .txt`

---
+++ frontend/app/manager/main/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+export default function Page() {
+    return (
+        <div>
+            <h1>Welcome to the Manager Page</h1>
+            <p>This is the main page of the manager section.</p>
+        </div>
+    );
+}   
